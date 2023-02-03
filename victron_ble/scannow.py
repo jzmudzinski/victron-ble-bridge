6b43f48929e3a2041f1db2a2183baec103534a2c
debueb <d.wisskirchen@gmail.com>
2023-02-03 13:33:12 +0100

    Create scannow.py

---
+++ victron_ble/scannow.py
@@ -0,0 +1,41 @@
+from scanreport import ScanReport
+import asyncio
+import click
+import logging
+from typing import List, Tuple
+
+logger = logging.getLogger(__name__)
+
+class DeviceKeyParam(click.ParamType):
+    name = "device_key"
+
+    def convert(self, value, param, ctx):
+        if isinstance(value, str):
+            parts = value.split("@")
+            if len(parts) == 2:
+                addr = parts[0].strip()
+                key = parts[1].strip()
+                return (addr, key)
+
+        self.fail(f"{value} is not a valid <addr>@<key> pair", param, ctx)
+
+@click.group()
+@click.option("-v", "--verbose", is_flag=True, help="Increase logging output")
+def cli(verbose):
+    logger.setLevel(logging.DEBUG if verbose else logging.INFO)
+
+
+@cli.command(help="Read data from specified devices")
+@click.argument("device_keys", nargs=-1, type=DeviceKeyParam())
+def read(device_keys: List[Tuple[str, str]]):
+    
+    async def scanAndReport(keys):
+        my_scanner = ScanReport(keys)
+        await my_scanner.run()
+
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(scanAndReport({k: v for k, v in device_keys}))
+
+
+if __name__ == "__main__":
+    cli()
